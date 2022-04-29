acde61654b312ae20dcf2725b97fbbdf52faa704
ravi4713 <4713rkr@gmail.com>
2022-04-30 01:35:02 +0530

    project files added

---
+++ accounts/views.py
@@ -0,0 +1,109 @@
+from django.shortcuts import render , redirect
+
+from django.contrib.auth.models import User
+
+from .models import Profile
+import random
+
+import http.client
+
+from django.conf import settings
+from django.contrib.auth import authenticate, login
+from twilio.rest import Client 
+# Create your views here.
+
+
+def send_otp(mobile , otp):
+    print("FUNCTION CALLED")
+    account_sid = 'AC714e55e46838412754d9ce598dc2b29f' 
+    auth_token = 'd6c12713d6a807302f3362918124e149'
+    client = Client(account_sid, auth_token) 
+    message = client.messages.create(
+        body = f'your otp is {otp}',
+        from_ = '+13322384997',
+        to=f'+91{mobile}')
+    print(message.sid)
+    return None
+
+    
+
+
+
+def login_attempt(request):
+    if request.method == 'POST':
+        mobile = request.POST.get('mobile')
+        
+        user = Profile.objects.filter(mobile = mobile).first()
+        
+        if user is None:
+            context = {'message' : 'User not found' , 'class' : 'danger' }
+            return render(request,'login.html' , context)
+        
+        otp = str(random.randint(1000 , 9999))
+        user.otp = otp
+        user.save()
+        send_otp(mobile , otp)
+        request.session['mobile'] = mobile
+        return redirect('login_otp')        
+    return render(request,'login.html')
+
+
+def login_otp(request):
+    mobile = request.session['mobile']
+    context = {'mobile':mobile}
+    if request.method == 'POST':
+        otp = request.POST.get('otp')
+        profile = Profile.objects.filter(mobile=mobile).first()
+        
+        if otp == profile.otp:
+            user = User.objects.get(id = profile.user.id)
+            login(request , user)
+            return redirect('cart')
+        else:
+            context = {'message' : 'Wrong OTP' , 'class' : 'danger','mobile':mobile }
+            return render(request,'login_otp.html' , context)
+    
+    return render(request,'login_otp.html' , context)
+    
+    
+
+def register(request):
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        name = request.POST.get('name')
+        mobile = request.POST.get('mobile')
+        
+        check_user = User.objects.filter(email = email).first()
+        check_profile = Profile.objects.filter(mobile = mobile).first()
+        
+        if check_user or check_profile:
+            context = {'message' : 'User already exists' , 'class' : 'danger' }
+            return render(request,'register.html' , context)
+            
+        user = User(email = email , first_name = name)
+        user.save()
+        otp = str(random.randint(1000 , 9999))
+        profile = Profile(user = user , mobile=mobile , otp = otp) 
+        profile.save()
+        send_otp(mobile, otp)
+        request.session['mobile'] = mobile
+        return redirect('otp')
+    return render(request,'register.html')
+
+def otp(request):
+    mobile = request.session['mobile']
+    context = {'mobile':mobile}
+    if request.method == 'POST':
+        otp = request.POST.get('otp')
+        profile = Profile.objects.filter(mobile=mobile).first()
+        
+        if otp == profile.otp:
+            return redirect('cart')
+        else:
+            print('Wrong')
+            
+            context = {'message' : 'Wrong OTP' , 'class' : 'danger','mobile':mobile }
+            return render(request,'otp.html' , context)
+            
+        
+    return render(request,'otp.html' , context)
